288cc03946884a3b472cf6a2014ff307690085a2
technicalpankajkumar <technicalpankajkumar@gamil.com>
2022-12-06 17:58:35 +0530

    major updates

---
+++ script.js
@@ -0,0 +1,23 @@
+           var no=document.querySelector("#no")
+           var no1=document.querySelector("#no1")
+           var no2=document.querySelector("#no2")
+           var no3=document.querySelector("#no3")
+	let laravel=()=>{
+           no2.style.display="none"
+           no3.style.display="none"
+           no.style.display="block"
+           no1.style.display="block"
+	}
+	let all_project=()=>{
+           no.style.display="block"
+           no1.style.display="block"
+           no2.style.display="block"
+           no3.style.display="block"
+	}
+	let HtmlCssJs=()=>
+	{
+		   no.style.display="none"
+		   no1.style.display="none"
+           no2.style.display="block"
+           no3.style.display="block"
+	}
